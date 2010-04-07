682cddf551d696451baaabdeeab87d16ae78d5f6
edhelms <baelout@gmail.com>
2010-04-07 14:51:30 -0400

    Removed vdi/ from all authentication urls.

--- idpauth/views.py
+++ idpauth/views.py
@@ -90,7 +90,7 @@ def openid_login(request):
         return HttpResponse('The OpenID was invalid')
 
     trust_root =  openid_tools.get_url_host(request) + '/'
-    redirect_to = openid_tools.get_url_host(request) + '/vdi/openid_login_complete/' + institution +'/'
+    redirect_to = openid_tools.get_url_host(request) + '/openid_login_complete/' + institution +'/'
 
     #Attribute Exchange
     requested_attributes = getattr(settings, 'OPENID_AX', False)
@@ -117,7 +117,7 @@ def openid_login_complete(request, institution):
 
     consumer = Consumer(request.session, openid_tools.DjangoOpenIDStore())
 
-    url = (openid_tools.get_url_host(request) + '/vdi/openid_login_complete/' + institution + '/').encode('utf8') + '?janrain_nonce=' + urllib.pathname2url(request.GET['janrain_nonce'])
+    url = (openid_tools.get_url_host(request) + '/openid_login_complete/' + institution + '/').encode('utf8') + '?janrain_nonce=' + urllib.pathname2url(request.GET['janrain_nonce'])
     query_dict = dict([
         (k.encode('utf8'), v.encode('utf8')) for k, v in request.GET.items()
     ])
@@ -147,7 +147,7 @@ def local_login(request):
     institution = request.POST['institution']
     user = authenticate(username=username, password=password)
 
-    roles = 'T'
+    roles = 'local'
 
     if user is not None:
         if user.is_active:
@@ -171,4 +171,5 @@ def shibboleth_login(request):
 def logout(request):
     session_institution = request.session["institution"]
     user_tools.logout(request)
-    return HttpResponseRedirect('/vdi/login/'+str(session_institution))
+    message = "You have successfully logged out"
+    return HttpResponseRedirect('/login/'+str(session_institution) )
