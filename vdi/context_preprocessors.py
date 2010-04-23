690dbc1ce63a6dc107a34d5b0315d316ef334e49
edhelms <baelout@gmail.com>
2010-04-23 15:39:36 -0400

    Edit to populate the local themeing.

--- vdi/context_preprocessors.py
+++ vdi/context_preprocessors.py
@@ -5,5 +5,10 @@ def context_preprocessor(request):
         'vdi_media_dir': settings.VDI_MEDIA_PREFIX,
     }
     if request.user.is_authenticated():
-        d['institution'] = request.user.username.split('++')[0]
+        institution = request.user.username.split('++')
+        if len(institution) == 1:
+            institution = 'local'
+        else:
+            institution = institution[0]
+        d['institution'] = institution
     return d
