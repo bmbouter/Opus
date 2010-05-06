8958fd7187a2f0452c7f4b469dd99947faad0111
Brian Bouterse <bmbouter@gmail.com>
2010-05-06 15:36:36 -0400

    Moved scale function out vdi.views and into vdi.tasks.
    Scale as a celery is preliminarily working, but not fully tested.

--- vdi/tasks.py
+++ vdi/tasks.py
@@ -6,28 +6,91 @@ import core
 log = core.log.getLogger()
 from django.http import HttpResponse, HttpResponseRedirect
 
-'''
-class MyPeriodicTask(Task):
-#class MyPeriodicTask(PeriodicTask):
-    #run_every = timedelta(seconds=30)
+class Scale(PeriodicTask):
+    run_every = timedelta(seconds=10)
 
-    def run(self, **kwargs):
-        log.debug("Running periodic task!")
+    def run(self):
+        for app in Application.objects.all():
+            # Create the cluster object to help us manage the cluster
+            cluster = AppCluster(app.pk)
 
-'''
+            # Clean up all idle users on all nodes for this application cluster
+            log.debug('APP NAME %s'%app.name)
+            cluster.logout_idle_users()
 
-class CreateUserTask(Task):
-    def run(self, username, password):
-        log.debug('8888888888888888888888')
+            # Handle vms we were waiting on to boot up
+            booting = deltacloud_tools.get_instances(cluster.booting)
+            for vm in booting:
+                dns_name = vm.public_addresses[0]
+                log.debug('ASDF = %s' % dns_name)
+                if dns_name.find("amazonaws.com") > -1:
+                    # Resolve the domain name into an IP address
+                    # This adds a dependancy on the 'host' command
+                    output = Popen(["host", dns_name], stdout=PIPE).communicate()[0]
+                    ip = '.'.join(re.findall('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', output)[0])
+                    try:
+                        # TODO: remove the hard coded '3389' & '22' below
+                        # TODO: remove the arbitrary '3' second timeout below
+                        socket.create_connection((ip,3389),3)
+                        socket.create_connection((ip,22),3)
+                    except Exception as e:
+                        log.error("Exception: %s" % e)
+                        pass
+                    else:
+                        instance = Instance.objects.filter(instanceId=vm.id)[0]
+                        booting.remove(vm)
+                        instance.ip = ip
+                        instance.state = 2
+                        instance.save()
+                        log.debug("Moving instance %s into enabled state with ip %s" % (instance.instanceId,ip))
+            num_booting = len(booting)
+            if num_booting > 0:
+                log.debug("Application cluster '%s' is still waiting for %s cluster nodes to boot" % (cluster.name,len(booting)))
 
-'''
-class MyTask(Task):
-    def run(self, some_arg, **kwargs):
-        logger = self.get_logger(**kwargs)
-        log.debug('*************')
-        logger.info('AEIOU')
-        logger.info("Did something: %s" % some_arg)
-'''
 
-tasks.register(CreateUserTask)
-#tasks.register(MyTask)
+            # Consider if the cluster needs to be scaled
+            log.debug('Considering %s app cluster for scaling ...' % cluster.name)
+            # Should I scale up?
+            log.debug('%s is avail (%s) < req (%s)?' % (cluster.app.name, cluster.avail_headroom, cluster.req_headroom))
+            if cluster.avail_headroom < cluster.req_headroom:
+                # Yes I should scale up
+                space_needed = cluster.req_headroom - cluster.avail_headroom
+                servers_needed = int(math.ceil(space_needed / float(cluster.app.users_per_small)))
+                log.debug('Available headroom (%s) is less than the cluster headroom goal (%s).  Starting %s additional cluster nodes now' % (cluster.avail_headroom,cluster.req_headroom,servers_needed))
+                for i in range(servers_needed):
+                    cluster.start_node()
+
+
+            # Handle instances we are supposed to shut down
+            toTerminate = []
+            for host in cluster.shutting_down:
+                log.debug('ASDASDASD    %s' % host.instanceId)
+                try:
+                    n = AppNode(host.ip)
+                    log.debug('AppNode %s is waiting to be shut down and has %s connections' % (host.ip,n.sessions))
+                    if n.sessions == []:
+                        toTerminate.append(host)
+                        host.shutdownDateTime = datetime.now()
+                        host.save()
+                except HostNotConnectableError:
+                    # Ignore this host that doesn't seem to be ssh'able, but log it as an error
+                    log.warning('AppNode %s is NOT sshable and should be looked into.  It is currently waiting to shutdown')
+            deltacloud_tools.terminate_instances(toTerminate)
+
+
+            # Should I scale down?
+            overprov_num = cluster.avail_headroom - cluster.req_headroom
+            log.debug('overprov (%s) avail (%s) required(%s)' % (overprov_num,cluster.avail_headroom,cluster.req_headroom))
+            # Reverse the list to try to remove the servers at the end of the waterfall
+            inuse_reverse = cluster.inuse_map
+            inuse_reverse.reverse()
+            for (host,inuse) in inuse_reverse:
+                # The node must have 0 sessions and the cluster must be able to be smaller while still leaving enough headroom
+                if int(inuse) == 0 and overprov_num >= cluster.app.users_per_small:
+                    overprov_num = overprov_num - cluster.app.users_per_small
+                    host.state = 4
+                    host.save()
+                    log.debug('Application Server %s has no sessions.  Removing that node from the cluster!' % host.ip)
+        return 'scaling complete @TODO put scaling event summary in this output'
+
+tasks.register(Scale)
--- vdi/views.py
+++ vdi/views.py
@@ -27,7 +27,7 @@ from vdi import deltacloud_tools
 from vdi.app_cluster_tools import AppCluster, AppNode, NoHostException
 import core
 log = core.log.getLogger()
-from vdi.tasks import CreateUserTask
+#from vdi.tasks import CreateUserTask
 #from vdi.tasks import MyTask
 from celery.decorators import task
 import cost_tools
@@ -41,96 +41,6 @@ def applicationLibrary(request):
     return render_to_response('vdi/application-library.html',
         {'app_library': db_apps},
         context_instance=RequestContext(request))
-
-def atest(request):
-    #tasks.register(MyPeriodicTask)
-    #MyTask.delay(some_arg="foo")
-    CreateUserTask.delay('bmbouter','apass')
-    return HttpResponse('OK')
-
-def scale(request):
-    for app in Application.objects.all():
-        # Create the cluster object to help us manage the cluster
-        cluster = AppCluster(app.pk)
-
-        # Clean up all idle users on all nodes for this application cluster
-        log.debug('APP NAME %s'%app.name)
-        cluster.logout_idle_users()
-
-        # Handle vms we were waiting on to boot up
-        booting = deltacloud_tools.get_instances(cluster.booting)
-        for vm in booting:
-            dns_name = vm.public_addresses[0]
-            log.debug('ASDF = %s' % dns_name)
-            if dns_name.find("amazonaws.com") > -1:
-                # Resolve the domain name into an IP address
-                # This adds a dependancy on the 'host' command
-                output = Popen(["host", dns_name], stdout=PIPE).communicate()[0]
-                ip = '.'.join(re.findall('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', output)[0])
-                try:
-                    # TODO: remove the hard coded '3389' & '22' below
-                    # TODO: remove the arbitrary '3' second timeout below
-                    socket.create_connection((ip,3389),3)
-                    socket.create_connection((ip,22),3)
-                except Exception as e:
-                    log.error("Exception: %s" % e)
-                    pass
-                else:
-                    instance = Instance.objects.filter(instanceId=vm.id)[0]
-                    booting.remove(vm)
-                    instance.ip = ip
-                    instance.state = 2
-                    instance.save()
-                    log.debug("Moving instance %s into enabled state with ip %s" % (instance.instanceId,ip))
-        num_booting = len(booting)
-        if num_booting > 0:
-            log.debug("Application cluster '%s' is still waiting for %s cluster nodes to boot" % (cluster.name,len(booting)))
-
-
-        # Consider if the cluster needs to be scaled
-        log.debug('Considering %s app cluster for scaling ...' % cluster.name)
-        # Should I scale up?
-        log.debug('%s is avail (%s) < req (%s)?' % (cluster.app.name, cluster.avail_headroom, cluster.req_headroom))
-        if cluster.avail_headroom < cluster.req_headroom:
-            # Yes I should scale up
-            space_needed = cluster.req_headroom - cluster.avail_headroom
-            servers_needed = int(math.ceil(space_needed / float(cluster.app.users_per_small)))
-            log.debug('Available headroom (%s) is less than the cluster headroom goal (%s).  Starting %s additional cluster nodes now' % (cluster.avail_headroom,cluster.req_headroom,servers_needed))
-            for i in range(servers_needed):
-                cluster.start_node()
-
-
-        # Handle instances we are supposed to shut down
-        toTerminate = []
-        for host in cluster.shutting_down:
-            log.debug('ASDASDASD    %s' % host.instanceId)
-            try:
-                n = AppNode(host.ip)
-                log.debug('AppNode %s is waiting to be shut down and has %s connections' % (host.ip,n.sessions))
-                if n.sessions == []:
-                    toTerminate.append(host)
-                    host.shutdownDateTime = datetime.now()
-                    host.save()
-            except HostNotConnectableError:
-                # Ignore this host that doesn't seem to be ssh'able, but log it as an error
-                log.warning('AppNode %s is NOT sshable and should be looked into.  It is currently waiting to shutdown')
-        deltacloud_tools.terminate_instances(toTerminate)
-
-
-        # Should I scale down?
-        overprov_num = cluster.avail_headroom - cluster.req_headroom
-        log.debug('overprov (%s) avail (%s) required(%s)' % (overprov_num,cluster.avail_headroom,cluster.req_headroom))
-        # Reverse the list to try to remove the servers at the end of the waterfall
-        inuse_reverse = cluster.inuse_map
-        inuse_reverse.reverse()
-        for (host,inuse) in inuse_reverse:
-            # The node must have 0 sessions and the cluster must be able to be smaller while still leaving enough headroom
-            if int(inuse) == 0 and overprov_num >= cluster.app.users_per_small:
-                overprov_num = overprov_num - cluster.app.users_per_small
-                host.state = 4
-                host.save()
-                log.debug('Application Server %s has no sessions.  Removing that node from the cluster!' % host.ip)
-    return HttpResponse('scaling complete @TODO put scaling event summary in this output')
 
 @login_required
 def connect(request,app_pk=None,conn_type=None):
