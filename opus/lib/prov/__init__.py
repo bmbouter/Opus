4b8717d73fc0046dccdf55614fedeb2445570fe6
Brian Bouterse <bmbouter@gmail.com>
2010-06-15 15:11:56 -0400

    Added get_prov() to opus.lib.prov package

--- opus/lib/prov/__init__.py
+++ opus/lib/prov/__init__.py
@@ -13,3 +13,14 @@ from state import State
 from storage_snapshot import StorageSnapshot
 from storage_volume import StorageVolume
 from transition import Transition
+
+def get_prov():
+    """Returns an instantiated Deltacloud object
+
+    The provider
+    """
+    from django.conf import settings
+    api_uri = settings.DELTACLOUD_API_URI
+    username = settings.DELTACLOUD_USERNAME
+    password = settings.DELTACLOUD_PASSWORD
+    return Deltacloud(username, password, api_uri)
