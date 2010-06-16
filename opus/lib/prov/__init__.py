4d9022ed625184c0cf0c4d996198243a3963a97a
Brian Bouterse <bmbouter@gmail.com>
2010-06-16 11:11:24 -0400

    Fixed a docstring and tested the opus.lib.prov.get_prov() method

--- opus/lib/prov/__init__.py
+++ opus/lib/prov/__init__.py
@@ -15,10 +15,16 @@ from storage_volume import StorageVolume
 from transition import Transition
 
 def get_prov():
-    """Returns an instantiated Deltacloud object
+    """Returns an pre-configured Deltacloud object
 
-    The provider
+    The returned Deltacloud object is tied to the deltacloud provider
+    indicated in the settings file using the following variables
+
+    DELTACLOUD_API_URI -- The primary entry point URI for the provider
+    DELTACLOUD_USERNAME -- The username to logon to the provider
+    DELTACLOUD_PASSWORD -- The password to logon to the provider
     """
+
     from django.conf import settings
     api_uri = settings.DELTACLOUD_API_URI
     username = settings.DELTACLOUD_USERNAME
