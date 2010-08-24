52adbc52895b788103fd30b40e9639c1e6f4d604
unknown <Eric@.(none)>
2010-08-24 16:43:32 -0400

    Small fix.

--- gwt/src/opus/gwt/management/console/client/ManagementConsole.java
+++ gwt/src/opus/gwt/management/console/client/ManagementConsole.java
@@ -82,6 +82,7 @@ public class ManagementConsole extends Composite {
 	
 	public ManagementConsole() {
 		initWidget(uiBinder.createAndBindUi(this));
+		mainDeckPanel.setAnimationEnabled(true);
 		loginPanel = new Login(mainDeckPanel, this);
 		iconPanel = new IconPanel(this);
 		JSVarHandler = new JSVariableHandler();
